da2742f0f6d7653c1804a8bdd6524a97c4e711eb
Mohd Kaif <kaifmohd2014@gmail.com>
2024-01-13 17:34:57 +0530

    bottom navigation completed

---
+++ lib/navigation_menu.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
+import 'package:iconsax/iconsax.dart';
+import 'package:medihealth/features/authentication/screens/home/home_screen.dart';
+import 'package:medihealth/utils/constants/colors.dart';
+import 'package:medihealth/utils/helpers/helper_functions.dart';
+
+class NavigationMenu extends StatelessWidget {
+  const NavigationMenu({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final controller = Get.put(NavigationController());
+    final darkMode = THelperFunctions.isDarkMode(context);
+    return Scaffold(
+      bottomNavigationBar: Obx(
+        () => NavigationBar(
+            height: 80,
+            elevation: 0,
+            selectedIndex: controller.selectedIndex.value,
+            onDestinationSelected: (index) =>
+                controller.selectedIndex.value = index,
+            backgroundColor: darkMode ? TColors.black : TColors.white,
+            indicatorColor: darkMode
+                ? TColors.white.withOpacity(0.1)
+                : TColors.black.withOpacity(0.1),
+            destinations: const [
+              NavigationDestination(
+                icon: Icon(Iconsax.home),
+                label: 'Home',
+              ),
+              NavigationDestination(
+                icon: Icon(Iconsax.shop),
+                label: 'Store',
+              ),
+              NavigationDestination(
+                icon: Icon(Iconsax.heart),
+                label: 'Wishlist',
+              ),
+              NavigationDestination(
+                icon: Icon(Iconsax.user),
+                label: 'Profile',
+              ),
+            ]),
+      ),
+      body: Obx(() => controller.screens[controller.selectedIndex.value]),
+    );
+  }
+}
+
+class NavigationController extends GetxController {
+  final Rx<int> selectedIndex = 0.obs;
+
+  final screens = [
+    HomeScreen(),
+    Container(
+      color: Colors.blue,
+    ),
+    Container(
+      color: Colors.green,
+    ),
+    Container(
+      color: Colors.yellow,
+    ),
+  ];
+}
